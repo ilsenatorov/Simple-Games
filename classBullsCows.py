e805d6f72ee556e3085bbab9122c65805074a67b
ilsenatorov <il.senatorov@gmail.com>
2017-11-23 10:19:15 +0000

    work continued

--- classBullsCows.py
+++ classBullsCows.py
@@ -7,24 +7,44 @@ import argparse
 parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument("-difficulty", help="choose the difficulty:\n1 = easy\n2 = medium\n3 = hard\n0 = debug", type=int)
 args = parser.parse_args()
+print(args.difficulty)
 
-if args.difficulty == 1:
-  print("Your number contains 3 digits")
-  NumLength = 3
-  Tries = 8
-elif args.difficulty == 2:
-  print("Your number contains 4 digits")
-  Tries = 7
-  NumLength = 4
-elif args.difficulty == 3:
-  print("Your number contains 5 digits")
-  NumLength = 5
-  Tries = 10
-elif args.difficulty == 0:
-  print("Welcome to debug")#DEBUGGING MODE
-  NumLength = 3
-  Tries = 3
-else:
-  print("You chose the default difficulty - medium. Your number contains 4 digits")
-  Tries = 7
-  NumLength = 4
+class Number(object):
+    def __init__(self, numlist):
+        self.numlist = numlist
+    def checkBulls(self, guess):
+        Bulls = 0
+        integer = 0
+        for c in guess:
+            if c == self.numlist[integer]:
+                Bulls += 1
+            integer += 1
+        return Bulls
+    def checkCows(self, guess):
+        Cows = 0
+        for c in guess:
+            if c in self.numlist:
+                Cows += 1
+        return Cows - self.checkBulls(guess)
+
+
+class Guess(object):
+    def __init__(self, guesslist):
+        self.guesslist = guesslist
+    def HasRepeats(self):
+        return len(self.guesslist) != len(set(self.guesslist))
+    def CheckLength(self, numlist):
+        return len(self.guesslist) == len(numlist)
+
+
+
+
+
+number = Number([1,2,3,4])
+guess = Guess(list(input("Please enter your guess: ")))
+print(number.numlist)
+print(guess.guesslist)
+print("Repeats: " + str(guess.HasRepeats()))
+print("Same Length: " + str(guess.CheckLength(number.numlist)))
+print("Bulls: " + str(number.checkBulls(guess.guesslist)))
+print("Cows: " + str(number.checkCows(guess.guesslist)))
